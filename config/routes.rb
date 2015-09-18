a0e28f334971c55c5563c1d0cd4350aee040708c
Bret Doucette <doucette.bret@gmail.com>
2015-09-17 19:01:59 -0600

    Fixed Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create, :show]
 
-
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#delete'
